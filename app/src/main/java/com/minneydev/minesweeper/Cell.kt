19799da3f4db8c23ab0611f6d42acba71df4f2de
John Minney <jminneyiii@gmail.com>
2020-04-29 21:59:00 -0600

    Working Commit Still need to add the alg for choosing spaces, and bombs to end the game I will add flags after that.

--- app/src/main/java/com/minneydev/minesweeper/Cell.kt
+++ app/src/main/java/com/minneydev/minesweeper/Cell.kt
@@ -1,13 +1,42 @@
 package com.minneydev.minesweeper
 
-class Cell (private val isClicked: Boolean, private  val r: Int, private  val c: Int) {
-    val isBomb = false    //cell is a bomb
-    val isFlagged = false //cell is flagged
-    val numAdjBombs = 0   //bombs near cell
+class Cell (private var cl: Boolean, private  val r: Int, private  val c: Int) {
+    var isClicked = cl
+    var isBomb = false    //cell is a bomb
+    var isFlagged = false //cell is flagged
+    var adjCells = mutableListOf<Cell>()
+    var hasEx = false
+    private var numAdjBombs = 0   //bombs near cell
+
+    private fun getAdjCells(cell: Cell) : List<Cell> {
+        var adjCells = mutableListOf<Cell?>()
+        if (r in 1..8) {
+            if (c in 1..8) {
+                adjCells.add(MainActivity.cells[r-1][c-1])
+                adjCells.add(MainActivity.cells[r-1][c])
+                adjCells.add(MainActivity.cells[r-1][c+1])
+                adjCells.add(MainActivity.cells[r][c-1])
+                adjCells.add(MainActivity.cells[r][c+1])
+                adjCells.add(MainActivity.cells[r+1][c-1])
+                adjCells.add(MainActivity.cells[r+1][c])
+                adjCells.add(MainActivity.cells[r+1][c+1])
+            }
+        }
+        return adjCells.toList().filterNotNull()
+    }
 
     fun getCellType() : Int {
         if (isClicked) {
             if (!isBomb) {
+                if (!hasEx) {
+                    val adjCells: List<Cell> = getAdjCells(this)
+                    adjCells.forEach() {
+                        if (it.isBomb) {
+                            numAdjBombs++
+                        }
+                    }
+                    hasEx = true
+                }
                 when (numAdjBombs) {
                     0 -> return R.drawable.cell_0
                     1 -> return R.drawable.cell_1
@@ -27,3 +56,6 @@ class Cell (private val isClicked: Boolean, private  val r: Int, private  val c:
     }
 
 }
+
+
+
--- app/src/main/java/com/minneydev/minesweeper/MainActivity.kt
+++ app/src/main/java/com/minneydev/minesweeper/MainActivity.kt
@@ -2,11 +2,61 @@ package com.minneydev.minesweeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.GridView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.grid_cell.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var gridAdapter: BoardGridAdapter
+    companion object {
+        lateinit var cells: Array<Array<Cell>> //2d array to make the board easier to work with
+        const val NUM_COL = 10
+        const val NUM_BOMB = 10
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        board_grid.numColumns = NUM_COL
+        reset_btn.text = getString(R.string.reset)
+        initialize()
+        board_grid.onItemClickListener = AdapterView.OnItemClickListener { _, v, position, _ ->
+            showCell(cells.flatten()[position])
+            v.cell.setImageResource(cells.flatten()[position].getCellType())
+        }
+        //reset button click listener
+        reset_btn.setOnClickListener {
+            initialize()
+        }
     }
+
+    private fun initialize() {
+        cells = Array(NUM_COL) {row ->
+            Array(NUM_COL) {col ->
+                Cell(false,row,col)
+            }
+        }
+        //Set Bombs
+        for (i in 0..NUM_BOMB-1) {
+            val row: Int = kotlin.random.Random.nextInt(0, NUM_COL)
+            val col: Int = kotlin.random.Random.nextInt(0, NUM_COL)
+            cells[row][col].isBomb = true
+        }
+        gridAdapter = BoardGridAdapter(this,cells.flatten())
+        board_grid.adapter = gridAdapter
+        title_txtview.text = getString(R.string.app_name)
+    }
+    private fun showCell(cell: Cell) {
+        cell.isClicked = true
+    }
+
 }
+
+        /*
+        board_grid.onItemClickListener = AdapterView.OnItemClickListener { _, v, position, _ ->
+            showCell(Constants.cells.flatten()[position])
+            v.cell.setImageResource(Constants.cells.flatten()[position].getImage())
+        }
+        */

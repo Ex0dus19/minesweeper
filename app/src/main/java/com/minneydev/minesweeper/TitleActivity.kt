4f41715464e10600f103f95a61ce318a08f522d0
John Minney <jminneyiii@gmail.com>
2020-05-19 15:26:09 -0600

    Worked Out Title and added Icon

--- app/src/main/java/com/minneydev/minesweeper/TitleActivity.kt
+++ app/src/main/java/com/minneydev/minesweeper/TitleActivity.kt
@@ -1,12 +1,27 @@
 package com.minneydev.minesweeper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_title.*
 
 class TitleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title)
+
+        val playButton = play_btn
+        playButton.setOnClickListener {
+            val playIntent = Intent(this, MainActivity::class.java)
+            startActivity(playIntent)
+        }
+
+        val settingsButton = settings_btn
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
